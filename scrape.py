7b033edd3d0f80891764a02336c321147ba1d014
Adam Abdulkadir <adama1862@outlook.com>
2022-09-14 21:49:38 -0400

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -4,9 +4,20 @@ import pprint
 
 # Hacker News
 res = requests.get('https://news.ycombinator.com/news')
+res2 = requests.get('https://news.ycombinator.com/news?p=2')
 soup = BeautifulSoup(res.text, 'html.parser')
+soup2 = BeautifulSoup(res2.text, 'html.parser')
+
 links = soup.select('.titlelink')
 subtext = soup.select('.subtext')
+links2 = soup2.select('.titlelink')
+subtext2 = soup2.select('.subtext')
+
+mega_links = links + links2
+mega_subtext = subtext +  subtext2
+
+def sort_stories_by_votes(hnlist):
+    return sorted(hnlist, key= lambda k:k['votes'], reverse=True)
 
 def custom(links,subtext):
     hn = []
@@ -16,7 +27,8 @@ def custom(links,subtext):
         vote = subtext[indx].select('.score')
         if len(vote):
             points = int(vote[0].getText().replace(' points', ''))
-            hn.append({'title': title, 'link': href , 'votes': points})
-    return hn
+            if points > 99:
+                hn.append({'title': title, 'link': href , 'votes': points})
+    return sort_stories_by_votes(hn)
 
-pprint.pprint(custom(links,subtext))
+pprint.pprint(custom(mega_links,mega_subtext))
